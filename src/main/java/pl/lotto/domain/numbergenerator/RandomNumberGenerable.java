74fcc78f2e0c24cb1ff78fd878656f25ed23c083
Wojciech Wasylewski <w.wasylewski@gmail.com>
2023-10-31 13:07:02 +0100

    Chenge requiremennts now we use external server to generate 6 numbers

--- src/main/java/pl/lotto/domain/numbergenerator/RandomNumberGenerable.java
+++ src/main/java/pl/lotto/domain/numbergenerator/RandomNumberGenerable.java
@@ -1,7 +1,5 @@
 package pl.lotto.domain.numbergenerator;
 
-import java.util.Set;
-
 public interface RandomNumberGenerable {
-    Set<Integer> generateSixRandomNumbers();
+    SixRandomNumbersDto generateSixRandomNumbers();
 }
